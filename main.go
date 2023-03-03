32c08df2aac3dacef3c69dbcd2de9ae332418c16
Keyu <me@keyu.email>
2023-03-03 13:58:53 +0800

    feat: add static file router

--- main.go
+++ main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"embed"
+	"io/fs"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.String(http.StatusOK, "<h1>Free-Transfer</h1>")
+		staticFiles, err := fs.Sub(FS, "frontend/dist")
+		if err != nil {
+			log.Fatal(err)
+		}
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(ctx *gin.Context) {
+			path := ctx.Request.URL.Path
+			if strings.HasPrefix(path, "/static") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				ctx.DataFromReader(http.StatusOK, stat.Size(), "text/html;charset=utf-8", reader, nil)
+			} else {
+				ctx.Status(http.StatusNotFound)
+			}
 		})
 		router.Run(":8080")
 	}()
-	fmt.Println("here--------------")
-	ui, _ := lorca.New("http://127.0.0.1:8080", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ := lorca.New("http://127.0.0.1:8080/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 	select {
